48a6516d73a953353fa39acfc7e7d34f26e83e6d
prathameshrr <prathamesh.patel15@gmail.com>
2022-08-01 15:44:51 +0530

    name change

--- p1.go
+++ p1.go
@@ -1,16 +1,15 @@
-package main 
+package main
 
 import "fmt"
+
 var sum int
- 
+
 func main() {
-	
+
 	for i := 1; i < 1000; i++ {
-		if i % 3 == 0 || i % 5 == 0 {
+		if i%3 == 0 || i%5 == 0 {
 			sum += i
 		}
 	}
 	fmt.Println(sum)
 }
-
-
